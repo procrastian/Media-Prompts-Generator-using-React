14586f6b0f126eec36237916e097cb42a3ece361
Ian Grantham <dev.iangrantham@gmail.com>
2023-05-30 17:47:09 +0100

    set up gh pages to allow use without local hosting

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import prompts from "../../db/db.js";
 
 export default function Home() {
-  const [promptList, setPromptList] = useState([]);
   const [prompt, setPrompt] = useState("");
 
-  //fetch the data from data.json
-  useEffect(() => {
-    fetch("http://localhost:4000/prompts")
-      .then((res) => res.json())
-      .then((data) => setPromptList(data));
-  }, [setPromptList]);
-
   const handleClick = () => {
-    //generate a random number between 1 and limit
-    const promptNum = Math.floor(Math.random() * promptList.length + 1);
-    //find the prompt at that position in json
-    promptList.find((item) => {
+    const promptNum = Math.floor(Math.random() * prompts.length + 1);
+    prompts.find((item) => {
       if (item.num === `${promptNum}`) {
-        //save to prompt state
         return setPrompt(item.prompt);
       }
     });
